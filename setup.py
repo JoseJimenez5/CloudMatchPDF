86882b8416367f1c0ee8fae70d7eb07cf0e23922
JoseJimenez5 <eluniversodelasecuaciones@gmail.com>
2026-03-11 00:55:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="cloud-match-pdf",
+    name="cloudlatex",
     version="0.1.1",
     author="Jose Jimenez",
     author_email="jose.jimenez05@epn.edu.ec", 
